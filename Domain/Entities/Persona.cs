bc1fe2102b5e7957b9023c8a5f33f4cde491f72d
Alejandro-Ariel-Salas <10alejandro4salas@gmail.com>
2026-04-15 16:41:39 -0300

    Pequeñas configuraciones y primera tabla

---
+++ Domain/Entities/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class Persona
+    {
+        public int Id { get; set; }
+
+        public int Dni { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Genero { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Localidad { get; set; }
+
+        public string CodigoPostal { get; set; }
+
+        public string Provincia { get; set; }
+
+        public string Pais { get; set; }
+
+        public string ContactoEmergencia { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
---
+++ Infraestructure/Persistence/EntityConfigurations/PersonaConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infraestructure.Persistence.EntityConfigurations
+{
+    public class PersonaConfiguration : IEntityTypeConfiguration<Persona>
+    {
+        public void Configure(EntityTypeBuilder<Persona> entity)
+        {
+            // Clave primaria
+            entity.HasKey(p => p.Id);
+
+            // Discriminador (herencia TPH)
+            //entity.HasDiscriminator<string>("TipoPersona")
+            //    .HasValue<Cliente>("Cliente")
+            //    .HasValue<Profesor>("Profesor")
+            //    .HasValue<Administrador>("Administrador");
+
+            // Propiedades obligatorias
+            entity.Property(p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(p => p.Apellido)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            entity.Property(p => p.Dni)
+                .IsRequired();
+
+            entity.Property(p => p.Telefono)
+                .HasMaxLength(20);
+
+            entity.Property(p => p.Direccion)
+                .HasMaxLength(200);
+
+            entity.Property(p => p.Localidad)
+                .HasMaxLength(100);
+
+            entity.Property(p => p.Provincia)
+                .HasMaxLength(100);
+
+            entity.Property(p => p.Pais)
+                .HasMaxLength(100);
+
+            entity.Property(p => p.CodigoPostal)
+                .HasMaxLength(20);
+
+            entity.Property(p => p.ContactoEmergencia)
+                .HasMaxLength(150);
+
+            entity.Property(p => p.FechaNacimiento)
+                .IsRequired();
+
+            entity.Property(p => p.FechaRegistro)
+                .HasDefaultValueSql("GETDATE()");
+
+            // Índice único
+            entity.HasIndex(p => p.Dni)
+                .IsUnique();
+        }
+    }
+}
