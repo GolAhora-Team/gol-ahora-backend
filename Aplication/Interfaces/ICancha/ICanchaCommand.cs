e1b374d6b25fced268b70fedd0642c9b647db312
Javier Florentin <javier.florentin.pri@gmail.com>
2026-04-25 16:06:18 -0300

    Se hicieron las estructuras del crud de cancha y precio con inyección de dependencias en el program, faltan las implementaciones de cada uno

---
+++ Aplication/Interfaces/ICancha/ICanchaCommand.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Interfaces.ICancha
+{
+    public interface ICanchaCommand
+    {
+        Task InsertCancha(Cancha cancha);
+        Task UpdateCancha(Cancha cancha);
+        Task RemoveCancha(int idCancha);
+    }
+}
---
+++ Infraestructure/Command/CanchaCommand.cs
@@ -0,0 +1,42 @@
+﻿using Aplication.Interfaces.ICancha;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Command
+{
+    public class CanchaCommand : ICanchaCommand
+    {
+        private readonly AppDbContext _context;
+
+        public CanchaCommand(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task InsertCancha(Cancha cancha)
+        {
+            _context.Add(cancha);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveCancha(int idCancha)
+        {
+            var cancha = await _context.Canchas.FindAsync(idCancha);
+            if(cancha != null)
+            {
+                _context.Remove(cancha);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateCancha(Cancha cancha)
+        {
+            _context.Update(cancha);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
