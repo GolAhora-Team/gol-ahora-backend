31a761de75f35117ec8941c0d76b731facdcd4c7
Javier Florentin <javier.florentin.pri@gmail.com>
2026-05-20 16:24:35 -0300

    Agrego EstadoPartido y FaseTorneo a la entidad partido con sus respectivos enums y migración

---
+++ Domain/Enums/FaseTorneo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum FaseTorneo
+    {
+        NoAplica = 0,
+        OctavosDeFinal = 1,
+        CuartosDeFinal = 2,
+        Semifinal = 3,
+        Final = 4
+    }
+}
